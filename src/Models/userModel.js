a29f283b8fac0ce6545192d195b6d68e7d120fea
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2022-07-06 11:56:06 +0530

    Boiler plate

--- src/Models/userModel.js
+++ src/Models/userModel.js
@@ -4,6 +4,29 @@ const userSchema = new mongoose.Schema({
     title: {
             type: String,
             required: true,
-            
+            enum: ["Mr", "Mrs", "Miss"]
+    },
+    name: {
+            type: String,
+            required: true
+    },
+    phone: {
+            type: String,
+            required: true,
+            unique:true
+    },
+    email: {
+            type:String,
+            required: true,
+            unique: true
+    },
+    password: {
+            type: String,
+            required: true,
+            minLength: 8,
+            maxLength: 15
+    },
+    address: {
+                
     }
 })
